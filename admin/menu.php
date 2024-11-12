cbb062d39a6a0e20757dc11bda3e840d63910da3
tad0616 <tad0616@gmail.com>
2024-11-12 16:24:58 +0800

    簡化後台選單陣列，修改釋出日期

--- admin/menu.php
+++ admin/menu.php
@@ -1,22 +1,18 @@
 <?php
-$adminmenu = array();
-
-$i = 1;
-$adminmenu[$i]['title'] = _MI_TAD_ADMIN_HOME;
-$adminmenu[$i]['link'] = 'admin/index.php';
-$adminmenu[$i]['icon'] = 'images/admin/home.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TADUSERS_ADMENU1;
-$adminmenu[$i]['link'] = "admin/main.php";
-$adminmenu[$i]['icon'] = "images/admin/review.png";
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TADUSERS_ADMENU2;
-$adminmenu[$i]['link'] = "admin/add.php";
-$adminmenu[$i]['icon'] = "images/admin/add.png";
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TAD_ADMIN_ABOUT;
-$adminmenu[$i]['link'] = 'admin/about.php';
-$adminmenu[$i]['icon'] = 'images/admin/about.png';
+$adminmenu = [
+    [
+        'title' => _MI_TADUSERS_ADMENU1,
+        'link' => 'admin/main.php',
+        'icon' => 'images/admin/review.png',
+    ],
+    [
+        'title' => _MI_TADUSERS_ADMENU2,
+        'link' => 'admin/add.php',
+        'icon' => 'images/admin/add.png',
+    ],
+    [
+        'title' => _MI_TAD_ADMIN_ABOUT,
+        'link' => 'admin/about.php',
+        'icon' => 'images/admin/about.png',
+    ],
+];
